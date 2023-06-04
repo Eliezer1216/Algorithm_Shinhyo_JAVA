41eaa8c8de20a146bed936b3f8e5b7ba32b792e8
(shinhyo) <(tlsgyals1216@gmail.com)>
2023-06-04 22:12:16 +0900

    Bronze2 8958번 문제 완료

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
 //여기는 코드 시험하는 구간
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        scanner.nextLine();
+        for(int i=0;i<num;i++)
+        {
+            String str=scanner.nextLine();
+            calculate(str);
+        }
+
     }
+    public static void calculate(String str)
+    {
+        int sum=1;
+        int result=0;
+        for(int k=0;k<str.length();k++)
+        {
+            char c=str.charAt(k);
+            if(c=='O')
+            {
+                result=result+sum;
+                sum=sum+1;
+            }
+            else {
+                sum=1;
+            }
+        }
+
+
+        System.out.println(result);
+    }
+
 }
+
+
+
